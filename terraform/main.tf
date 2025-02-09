9b5eb6f07e3b4a65af26e1e5becb7022e99ccb3e
Kota Yuhara <hotwater1367@gmail.com>
2025-02-09 14:57:59 +0900

    update

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,9 @@
-provider "google" {
-  project = "aiagent-449708"
-  region  = "asia-northeast1"
+terraform {
+  required_version = ">= 1.9.4, < 2.0.0"
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.41.0"
+    }
+  }
 }
