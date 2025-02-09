8f0902509a5e7ee0463eb51c6d3f8f7613e67969
Kota Yuhara <hotwater1367@gmail.com>
2025-02-09 19:56:13 +0900

    add project_id and region

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,3 +2,13 @@ variable "project_name" {
   type    = string
   default = "videoflowai"
 }
+
+variable "project_id" {
+  type    = string
+  default = "aiagent-449708"
+}
+
+variable "region" {
+  type    = string
+  default = "asia-northeast1"
+}
